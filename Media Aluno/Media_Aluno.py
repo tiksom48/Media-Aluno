07891d76fad5c115d52990e92da15fa81ffc1a11
Jonathan patrich <tiksom_48@hotmail.com>
2021-12-10 21:23:22 -0300

    Adicionar arquivos de projeto.

---
+++ Media Aluno/Media_Aluno.py
@@ -0,0 +1,8 @@
+''' Programa Media '''
+
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+media = (nota1 + nota2) / 2
+if media >= 7:
+    print ("O aluno está aprovado")
+    print ("Parabéns !")
